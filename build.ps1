327d5545d3c95a86c52e08d5efb4c70ce5970ac2
Igor Stojković <igors@nordeus.com>
2019-05-10 17:38:47 +0200

    Replaced DotNetZip with Ionic.Zip.Reduced and updated to projects to Unity 2018.3.11

--- build.ps1
+++ build.ps1
@@ -37,8 +37,8 @@ if ( $LASTEXITCODE -ne 0 ) {
 
 
 # Copy .dlls from the build into the Packager folder
-Copy-Item ".\CreateDLL\bin\Release\NugetForUnity.dll" ".\Packager\Assets\NuGet\Editor"
-Copy-Item ".\CreateDLL\bin\Release\DotNetZip.dll" ".\Packager\Assets\NuGet\Editor"
+Copy-Item ".\CreateDLL\bin\Release\NugetForUnity.dll" ".\Packager\Assets\NuGet"
+Copy-Item ".\CreateDLL\bin\Release\Ionic.Zip.Reduced.dll" ".\Packager\Assets\NuGet"
 
 # Launch Unity to export the NuGetForUnity package
 Start-UnityEditor -Project ".\Packager" -BatchMode -Quit -Wait -ExportPackage "Assets/NuGet .\NuGetForUnity.unitypackage" -LogFile ".\Packager\NuGetForUnity.unitypackage.log"
@@ -46,5 +46,5 @@ Start-UnityEditor -Project ".\Packager" -BatchMode -Quit -Wait -ExportPackage "A
 # Copy artifacts to output directory
 if ( !(Test-Path $OutputDirectory) ) { New-Item -ItemType Directory $OutputDirectory  }
 Copy-Item ".\CreateDLL\bin\Release\NugetForUnity.*" $OutputDirectory
-Copy-Item ".\CreateDLL\bin\Release\DotNetZip.*" $OutputDirectory
+Copy-Item ".\CreateDLL\bin\Release\Ionic.Zip.Reduced.*" $OutputDirectory
 Copy-Item ".\Packager\NuGetForUnity.unitypackage*" $OutputDirectory
